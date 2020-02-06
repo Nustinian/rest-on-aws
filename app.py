d91e222d730ba560d89935716c59c0216b773eca
unknown <austin.w.atwood@gmail.com>
2020-02-06 20:56:18 +0900

    added token refresh functionality

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
 
-from resources.user import UserRegister, UserList, User, UserLogin
+from resources.user import UserRegister, UserList, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -33,6 +33,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,5 @@
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
@@ -50,3 +50,10 @@ class UserLogin(Resource):
 class UserList(Resource):
     def get(self):
         return {'users': [user.json() for user in UserModel.query.all()]}
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token' = new_token}
