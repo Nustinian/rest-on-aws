066c4e3c5810a91419a3e33f9d75682374039bd7
Austin <50373319+Nustinian@users.noreply.github.com>
2020-02-06 23:11:59 +0900

    trying to add refresh token into user claims of access token

--- resources/user.py
+++ resources/user.py
@@ -42,9 +42,9 @@ class UserLogin(Resource):
         data = _user_parser.parse_args()
         user = UserModel.find_by_username(data['username'])
         if user and safe_str_cmp(user.password, data['password']):
-            access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
-            return {'access_token': access_token, 'refresh_token': refresh_token}, 200
+            access_token = create_access_token(identity=user.id, fresh=True, user_claims={'refresh_token': refresh_token})
+            return {'access_token': access_token, 'refresh_token': access_token['refresh_token']}, 200
         return {'message': 'Invalid credentials.'}, 401
 
 
