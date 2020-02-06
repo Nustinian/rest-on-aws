51c73a15aaaf15354fd8f9c61a1528ac49d09c58
unknown <austin.w.atwood@gmail.com>
2020-02-06 21:37:03 +0900

    yup, don't include .py when importing...

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,7 @@ from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity, jwt_required, get_raw_jwt
 from flask_restful import Resource, reqparse
 from models.user import UserModel
-from blacklist.py import BLACKLIST
+from blacklist import BLACKLIST
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username', type=str, required=True, help="'username' field cannot be left blank.")
