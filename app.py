1819bb5ceb629c56468169b943ef4ba75b58af5d
unknown <austin.w.atwood@gmail.com>
2020-02-06 21:19:05 +0900

    added a bunch of custom callbacks

--- app.py
+++ app.py
@@ -19,6 +19,26 @@ api = Api(app)
 
 jwt = JWTManager(app)
 
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({"description": "The token has expired.", "error": "token_expired"}), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({"description": "Signature verification failed.", "error": "invalid_token"}), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({"description": "Request does not take an access token.", "error": "authorization_required"}), 401
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback(error):
+    return jsonify({"description": "This action requires a fresh token.", "error": "fresh_token_required"}), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({"description": "This token has been revoked.", "error": "token_revoked"}), 401
+
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:
