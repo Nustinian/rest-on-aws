3ddcd3ff5cd778efa20b18c97c23eba3e9c77cf0
unknown <austin.w.atwood@gmail.com>
2020-02-06 19:50:15 +0900

    fixed dictionary key

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ jwt = JWTManager(app)
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:
-        return {'is admin': True}
-    return {'is admin': False}
+        return {'is_admin': True}
+    return {'is_admin': False}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
