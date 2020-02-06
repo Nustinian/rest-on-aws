a574bec5f8983504509e3d645d098b2a10b4d273
unknown <austin.w.atwood@gmail.com>
2020-02-06 20:58:38 +0900

    fixed really dumb mistake! lots of these when trying to type fastgit add .

--- resources/user.py
+++ resources/user.py
@@ -56,4 +56,4 @@ class TokenRefresh(Resource):
     def post(self):
         current_user = get_jwt_identity()
         new_token = create_access_token(identity=current_user, fresh=False)
-        return {'access_token' = new_token}
+        return {'access_token': new_token}
